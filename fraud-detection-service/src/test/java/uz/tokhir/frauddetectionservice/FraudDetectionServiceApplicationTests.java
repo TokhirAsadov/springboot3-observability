df3eccdce155e98bda939d71f87b5f950f290424
Takhir <guvalakat1603@gmail.com>
2024-08-30 15:35:24 +0500

    fraud-detection-service module is added

---
+++ fraud-detection-service/src/test/java/uz/tokhir/frauddetectionservice/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tokhir.frauddetectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
