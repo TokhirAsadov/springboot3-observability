fa37fd1d6ae448dd782a593f12be93ba1adbdc7e
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:36:30 +0500

    FraudDetectionController controller is created

---
+++ fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/controller/FraudDetectionController.java
@@ -0,0 +1,17 @@
+package uz.tokhir.frauddetectionservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.tokhir.frauddetectionservice.service.FraudDetectionService;
+
+@RestController
+@RequestMapping("/fraud")
+@RequiredArgsConstructor
+@Slf4j
+public class FraudDetectionController {
+
+    private final FraudDetectionService fraudDetectionService;
+
+}
