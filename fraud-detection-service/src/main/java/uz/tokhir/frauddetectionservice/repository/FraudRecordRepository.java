01ce078a83b63ed7f676979852175cae2a2542b1
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:32:29 +0500

    existsByCustomerId function is done

--- fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/repository/FraudRecordRepository.java
+++ fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/repository/FraudRecordRepository.java
@@ -3,6 +3,7 @@ package uz.tokhir.frauddetectionservice.repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @RequiredArgsConstructor
@@ -10,5 +11,16 @@ public class FraudRecordRepository {
 
     private final JdbcClient jdbcClient;
 
-
+    @Transactional(readOnly = true)
+    public boolean existsByCustomerId(int customerId) {
+        var sql = """
+                SELECT COUNT(*) AS fraud__record_exists
+                FROM fraud_records
+                WHERE customerId = :customerId;
+                """;
+        return jdbcClient.sql(sql)
+                .param("customerId", customerId)
+                .query(Integer.class)
+                .single() > 0;
+    }
 }
