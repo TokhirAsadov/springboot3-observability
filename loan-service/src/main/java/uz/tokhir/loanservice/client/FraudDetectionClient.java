d5d3f603cce659f4172fd6ca0ce7753e95c0eeb8
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:18:43 +0500

    evaluateLoan

--- loan-service/src/main/java/uz/tokhir/loanservice/client/FraudDetectionClient.java
+++ loan-service/src/main/java/uz/tokhir/loanservice/client/FraudDetectionClient.java
@@ -2,8 +2,10 @@ package uz.tokhir.loanservice.client;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import uz.tokhir.loanservice.entity.LoanStatus;
 
 @Component
 @RequiredArgsConstructor
@@ -12,4 +14,12 @@ public class FraudDetectionClient {
 
     private final RestTemplate fraudServiceRestTemplate;
 
+    public LoanStatus evaluateLoan(int customerId) {
+        log.info("Calling Fraud Detection Service for customer id: {}", customerId);
+        var response = fraudServiceRestTemplate.exchange("/fraud/check?customerId=" + customerId,
+                        HttpMethod.GET, null, LoanStatus.class)
+                .getBody();
+        log.info("Fraud Detection Service response: {}", response);
+        return response;
+    }
 }
