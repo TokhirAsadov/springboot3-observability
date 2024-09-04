5dade46df077c86591b010a508ff5ddbdf0e1634
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:33:44 +0500

    observedAspect is beaned

--- fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/config/ObservationConfig.java
+++ fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/config/ObservationConfig.java
@@ -1,7 +1,16 @@
 package uz.tokhir.frauddetectionservice.config;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ObservationConfig {
+
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry registry) {
+        return new ObservedAspect(registry);
+    }
+
 }
