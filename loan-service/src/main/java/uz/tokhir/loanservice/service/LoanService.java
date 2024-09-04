8aecf29181434194b4a6f783bd13973b2811f49d
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:24:10 +0500

    applyLoan function is done

--- loan-service/src/main/java/uz/tokhir/loanservice/service/LoanService.java
+++ loan-service/src/main/java/uz/tokhir/loanservice/service/LoanService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.tokhir.loanservice.client.FraudDetectionClient;
 import uz.tokhir.loanservice.dto.LoanDto;
+import uz.tokhir.loanservice.entity.Loan;
+import uz.tokhir.loanservice.entity.LoanStatus;
 import uz.tokhir.loanservice.repository.LoanRepository;
 
 import java.util.List;
@@ -20,5 +22,16 @@ public class LoanService {
                 .stream()
                 .map(LoanDto::from)
                 .toList();
+    }
+
+    public String applyLoan(LoanDto loanDto) {
+        var loan = Loan.from(loanDto);
+        LoanStatus loanStatus = fraudDetectionClient.evaluateLoan(loan.getCustomerId());
+        loan.setLoanStatus(loanStatus);
+        if (loanStatus.equals(LoanStatus.APPROVED)) {
+            loanRepository.save(loan);
+            return "Loan applied successfully";
+        }
+        return "Sorry! Your loan was not approved";
     }
 }
