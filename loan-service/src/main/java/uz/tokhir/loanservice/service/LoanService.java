14a1936c50ed0f43949e6aa57493504f1ed0c8d5
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:23:51 +0500

    listAllLoans function is done

--- loan-service/src/main/java/uz/tokhir/loanservice/service/LoanService.java
+++ loan-service/src/main/java/uz/tokhir/loanservice/service/LoanService.java
@@ -3,7 +3,10 @@ package uz.tokhir.loanservice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.tokhir.loanservice.client.FraudDetectionClient;
+import uz.tokhir.loanservice.dto.LoanDto;
 import uz.tokhir.loanservice.repository.LoanRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +15,10 @@ public class LoanService {
     private final LoanRepository loanRepository;
     private final FraudDetectionClient fraudDetectionClient;
 
+    public List<LoanDto> listAllLoans() {
+        return loanRepository.findAll()
+                .stream()
+                .map(LoanDto::from)
+                .toList();
+    }
 }
