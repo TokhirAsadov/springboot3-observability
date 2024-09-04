08ae5d22973066ab32ba01a9df81da12a4864779
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:36:55 +0500

    checkForFraud api is done

--- fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/controller/FraudDetectionController.java
+++ fraud-detection-service/src/main/java/uz/tokhir/frauddetectionservice/controller/FraudDetectionController.java
@@ -2,8 +2,11 @@ package uz.tokhir.frauddetectionservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uz.tokhir.frauddetectionservice.entity.LoanStatus;
 import uz.tokhir.frauddetectionservice.service.FraudDetectionService;
 
 @RestController
@@ -14,4 +17,9 @@ public class FraudDetectionController {
 
     private final FraudDetectionService fraudDetectionService;
 
+    @GetMapping("/check")
+    public LoanStatus checkForFraud(@RequestParam int customerId) {
+        log.info("Checking for fraud for customer id: {}", customerId);
+        return fraudDetectionService.checkForFraud(customerId);
+    }
 }
