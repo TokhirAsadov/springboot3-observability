54365c0332f004c0c5175e4673c2129488e9b69f
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:22:23 +0500

    findAll query function is done

--- loan-service/src/main/java/uz/tokhir/loanservice/repository/LoanRepository.java
+++ loan-service/src/main/java/uz/tokhir/loanservice/repository/LoanRepository.java
@@ -4,6 +4,10 @@ import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import uz.tokhir.loanservice.entity.Loan;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,4 +16,9 @@ public class LoanRepository {
 
     private final JdbcClient jdbcClient;
 
+    @Transactional(readOnly = true)
+    public List<Loan> findAll() {
+        var findQuery = "SELECT id, loanId, customerName, customerId, amount, loanStatus FROM loans";
+        return jdbcClient.sql(findQuery).query(Loan.class).list();
+    }
 }
