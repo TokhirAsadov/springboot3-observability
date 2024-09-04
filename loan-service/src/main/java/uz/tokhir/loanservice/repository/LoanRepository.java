3bcd1f04b6864821f08d50edef3dd5f3e2b74376
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:22:47 +0500

    save query function is done

--- loan-service/src/main/java/uz/tokhir/loanservice/repository/LoanRepository.java
+++ loan-service/src/main/java/uz/tokhir/loanservice/repository/LoanRepository.java
@@ -3,11 +3,14 @@ package uz.tokhir.loanservice.repository;
 import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uz.tokhir.loanservice.entity.Loan;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,5 +23,19 @@ public class LoanRepository {
     public List<Loan> findAll() {
         var findQuery = "SELECT id, loanId, customerName, customerId, amount, loanStatus FROM loans";
         return jdbcClient.sql(findQuery).query(Loan.class).list();
+    }
+
+    @Transactional
+    public Long save(Loan loan) {
+        var insertQuery = "INSERT INTO loans(loanId, customerName, customerId, amount, loanStatus) VALUES(?, ?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcClient.sql(insertQuery)
+                .param(1, UUID.randomUUID().toString())
+                .param(2, loan.getCustomerName())
+                .param(3, loan.getCustomerId())
+                .param(4, loan.getAmount())
+                .param(5, loan.getLoanStatus().toString())
+                .update();
+        return keyHolder.getKeyAs(Long.class);
     }
 }
