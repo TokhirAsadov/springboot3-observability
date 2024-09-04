572e2a30d66487fe25b7a4c1b8c20fe4e8490403
Takhir <guvalakat1603@gmail.com>
2024-09-04 08:21:22 +0500

    fraudServiceRestTemplate is done

--- loan-service/src/main/java/uz/tokhir/loanservice/config/RestClientConfig.java
+++ loan-service/src/main/java/uz/tokhir/loanservice/config/RestClientConfig.java
@@ -1,7 +1,17 @@
 package uz.tokhir.loanservice.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
+
+    @Bean
+    RestTemplate fraudServiceRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .rootUri("http://localhost:8081")
+                .build();
+    }
 }
